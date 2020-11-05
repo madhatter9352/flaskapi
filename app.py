67583cd9d25134c17727ed1e409136559a53ed2c
lazaro <lachy9352@gmail.com>
2020-11-05 15:27:43 -0400

    includen the protection decorator to the rest of the routes

--- app.py
+++ app.py
@@ -61,6 +61,7 @@ def get_member(member_id):
 
 
 @app.route('/member', methods = ['POST'])
+@protected
 def add_member():
     if request.method == 'POST':
         member_data = request.get_json()
@@ -82,6 +83,7 @@ def add_member():
 
 
 @app.route('/member/<int:member_id>', methods = ['PUT', 'PATCH'])
+@protected
 def edit_member(member_id):
     db = get_bd()
 
@@ -104,6 +106,7 @@ def edit_member(member_id):
 
 
 @app.route('/member/<int:member_id>', methods = ['DELETE'])
+@protected
 def delete_member(member_id):
     db = get_bd()
     db.execute("delete from members where id = ?", [member_id])
